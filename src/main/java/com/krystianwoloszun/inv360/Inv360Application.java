654994b0868dcf810b955a0e4b5efb12f71118b4
Krystian Wołoszun <krystianwoloszun@gmail.com>
2026-06-04 18:37:08 +0200

    Initial commit

---
+++ src/main/java/com/krystianwoloszun/inv360/Inv360Application.java
@@ -0,0 +1,13 @@
+package com.krystianwoloszun.inv360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inv360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Inv360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/krystianwoloszun/inv360/Inv360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krystianwoloszun.inv360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Inv360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
